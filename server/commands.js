a006257ca2dd65a9056dc2c16879b71f8f59690e
Axel Duval <axel.duval74@gmail.com>
2025-04-02 18:00:11 +0200

    release

--- server/commands.js
+++
@@ -1,8 +0,0 @@
-export const COMMANDS = {
-    JOIN: 'join',
-    LEAVE: 'leave',
-    MESSAGE: 'message',
-    CONNECTED: 'connected',
-    DISCONNECTED: 'disconnected',
-    COLOR_UPDATE: 'color_update',
-};
